02929b6fb2328b988c47a539ff1303e3c4b69bf8
Shivraj Deepsagar <shivrajdeepsagar@Shivrajs-MacBook-Air.local>
2020-09-14 09:58:58 +0530

    routes for all the rest requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :cabs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cabs, only: :index, defaults: { format: :json } do
+      collection do
+        post 'book_nearest', to: 'cabs#book_nearest'
+      end
+  end  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :trips, only: [], defaults: { format: :json } do
+    member do
+      post 'start'
+      post 'end'
+    end
+  end
+
 end
